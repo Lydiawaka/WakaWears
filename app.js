d9a5ebc3ea056a3b6544b3febe1f21245ab0b9bf
Lydiawaka <economistwaka@gmail.com>
2024-09-24 22:49:12 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -19,17 +19,4 @@ document.querySelectorAll('.product-card button').forEach(button => {
     });
 });
 
-var MenuItems=document.getElementById("MenuItems");
-
-MenuItems.style.maxHeight = "0px";
-function menutoggle(){
-    if(MenuItems.style.maxHeight == "0px")
-{
-    MenuItems.style.maxHeight = "200px"
-}
-else
-{
-    MenuItems.style.maxHeight = "0px"
-}
-}
 
